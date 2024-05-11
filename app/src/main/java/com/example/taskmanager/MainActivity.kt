94fad4e40a60908cdf21a0c66e01746a70386df3
janithwijethunga <janithwijethunga68@gmail.com>
2024-05-12 03:14:54 +0530

    commit

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -5,8 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.taskmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
