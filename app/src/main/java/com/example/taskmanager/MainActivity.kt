e2d5eebc8c4a285cc099d75432ba26546fb19871
janithwijethunga <janithwijethunga68@gmail.com>
2024-05-14 16:52:19 +0530

    final product

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -2,12 +2,11 @@ package com.example.taskmanager
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.taskmanager.databinding.ActivityMainBinding
+import android.os.Handler
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +18,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+
 
         db = TasksDatabaseHelper(this)
         tasksAdapter = TasksAdapter(db.getAllTasks(), this)
---
+++ app/src/main/java/com/example/taskmanager/launching_screen.kt
@@ -0,0 +1,30 @@
+package com.example.taskmanager
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.os.Handler
+
+class launching_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_launching_screen)
+
+
+        Handler().postDelayed({
+            val intent = Intent(this@launching_screen,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3500)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
